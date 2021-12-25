1e36d3c88fd1e298e4acf3cd443b4be1a92abae7
Derek Holland <derekhollandwebdev@gmail.com>
2021-12-25 15:09:09 -0500

    calculate function
    finished most of the calculate function

--- js/site.js
+++ js/site.js
@@ -23,51 +23,70 @@ function refreshApp() {
 // write a function that gets all of the values
 function getValues() {
   // get the loan value
-  let balance = document.getElementById("loanValue").value;
+  let loan = document.getElementById("loanValue").value;
   // get the term value
   let term = document.getElementById("termValue").value;
   // get the interest rate value
   let rate = document.getElementById("interestValue").value;
   // convert values to integers
-  balance = parseInt(balance);
+  loan = parseInt(loan);
   term = parseInt(term);
   rate = parseInt(rate);
   // test if values are integers
   if (
-    Number.isInteger(balance) &&
+    Number.isInteger(loan) &&
     Number.isInteger(term) &&
     Number.isInteger(rate)
   ) {
     // call the calculate function and save returned results to a variable
-    mortgageData = calculateValues(balance, term, rate);
+    mortgageData = calculateValues(loan, term, rate);
   } else {
     alert("You must enter only integers");
   }
   // pass the mortgageData variable to displayData to show the results on the screen
 }
 
-function calculateValues(balance, term, rate) {
+function calculateValues(loan, term, rate) {
   // create the array of data showing the amorization schedule for each month
-  let mortgageData;
+  let mortgageData = [];
+
   // Total Monthly Payment = (amount loaned) * (interestRate/1200) / (1 – (1 + interestRate/1200)
   let monthlyPayment =
-    (balance * (rate / 1200)) / (1 - (1 + rate / 1200) ** -term);
-  console.log(`monthly payment is ${monthlyPayment}`);
+    (loan * (rate / 1200)) / (1 - (1 + rate / 1200) ** -term);
   // Remaining Balance before the very first month equals the amount of the loan.
-  let remainingBalance = balance;
-  console.log(`remaining balance is: ${remainingBalance}`);
+  let remainingBalance = loan;
+  //These variables are predicated on the remaining Balance changing for each iteration
   // Interest Payment = Previous Remaining Balance * rate/1200
-  let interestPayment = remainingBalance * (rate / 1200);
-  console.log(`interest payment is: ${interestPayment}`);
+  let interestPayment = 0;
   // Principal Payment = Total Monthly Payment - Interest Payment
-  let principalPayment = monthlyPayment - interestPayment;
-  console.log(`principal payment is: ${principalPayment}`);
-  // At end each month, Remaining Balance = Previous Remaining Balance - principal payments
-  let endOfMonthBalance = remainingBalance - principalPayment;
-  console.log(
-    `The balance at the end of each month is is: ${endOfMonthBalance}`
-  );
-  // create the array of data showing the amorization schedule for each month
+  let principalPayment = 0;
+
+  // loop through the length of the term and calculate monthly data
+  for (let i = 1; i <= term; i++) {
+    // At end each month, Remaining Balance = Previous Remaining Balance - principal payments
+    remainingBalance = remainingBalance - principalPayment;
+    if (remainingBalance <= 0) {
+      remainingBalance = 0;
+    }
+    interestPayment = remainingBalance * (rate / 1200);
+    principalPayment = monthlyPayment - interestPayment;
+    //create data objects for each month
+
+    const month = {
+      month: i,
+      payment: monthlyPayment.toFixed(2),
+      interest: interestPayment.toFixed(2),
+      principal: principalPayment.toFixed(2),
+      total: remainingBalance.toFixed(2),
+    };
+    console.log(month);
+    mortgageData.push(month);
+  }
+  let totalPrincipal;
+  let totalInterest;
+  let totalCost;
+
+  return mortgageData;
 }
 
 //custom display function
@@ -75,7 +94,7 @@ function displayData(mortgageData) {
   //get the table body element from the page
   let tableBody = document.getElementById("results");
   //get the template row
-  let templateRow = document.getElementById("fbTemplate");
+  let templateRow = document.getElementById("lsTemplate");
   //clear table first
   tableBody.innerHTML = "";
 
@@ -88,20 +107,24 @@ function displayData(mortgageData) {
     let rowCols = tableRow.querySelectorAll("td");
     // add the respective class to each column to change the styling
     // set the data for each column
-    rowCols[0].classList.add(mortgageData[i]);
+    // rowCols[0].classList.add(mortgageData[i]);
     rowCols[0].textContent = mortgageData[i];
     //
-    rowCols[1].classList.add(mortgageData[i + 1]);
+    // rowCols[1].classList.add(mortgageData[i + 1]);
     rowCols[1].textContent = mortgageData[i + 1];
     //
-    rowCols[2].classList.add(mortgageData[i + 2]);
+    // rowCols[2].classList.add(mortgageData[i + 2]);
     rowCols[2].textContent = mortgageData[i + 2];
     //
-    rowCols[3].classList.add(mortgageData[i + 3]);
+    // rowCols[3].classList.add(mortgageData[i + 3]);
     rowCols[3].textContent = mortgageData[i + 3];
     //
-    rowCols[4].classList.add(mortgageData[i + 4]);
+    // rowCols[4].classList.add(mortgageData[i + 4]);
     rowCols[4].textContent = mortgageData[i + 4];
+    //
+    // rowCols[4].classList.add(mortgageData[i + 5]);
+    rowCols[4].textContent = mortgageData[i + 5];
+
     // add the template to the document
     tableBody.appendChild(tableRow);
   }
