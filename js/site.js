2fdf0f911807119b0d312dfe41ef5a4d0f82268b
Derek Holland <derekhollandwebdev@gmail.com>
2021-12-26 20:51:35 -0500

    responsive design
    added bootstrap breakpoints

--- js/site.js
+++ js/site.js
@@ -1,18 +1,5 @@
 // Mortgage Loan Calculator
 
-// output should include the following
-// the month  (corresponding to the 1st month of payment, through the total number of months )
-// the payments amount
-// the principal paid this month
-// the interest paid this month
-// the total interest paid this month
-// the remaining loan balance at the end of the month
-
-// Write a mortgage loan calculator application that takes in three parameters:
-// 1. The amount of money loaned in dollars (balance)
-// 2. The time over which the loan will be repaid, in months (term)
-// 3. The percentage rate at which interest will accrue on the loan (rate)
-
 // write a mortgage loan calculator that takes loan amount, months, and interest rate and prints out the amorization schedule which includes month by month the month, payment, principal, interest, total interest, and balance remaining
 
 // refresh button
@@ -48,16 +35,13 @@ function getValues() {
 function calculateValues(loan, term, rate) {
   // create the array of data showing the amorization schedule for each month
   let scheduleData = [];
-
   // Total Monthly Payment = (amount loaned) * (interestRate/1200) / (1 – (1 + interestRate/1200)
   let monthlyPayment =
     (loan * (rate / 1200)) / (1 - (1 + rate / 1200) ** -term);
   // Remaining Balance before the very first month equals the amount of the loan.
   let remainingBalance = loan;
-  //These variables are predicated on the remaining Balance changing for each iteration
-  // Interest Payment = Previous Remaining Balance * rate/1200
+  //Create variables to store the calculations
   let interestPayment = 0;
-  // Principal Payment = Total Monthly Payment - Interest Payment
   let principalPayment = 0;
   let totalInterest = 0;
   let totalCost = 0;
@@ -67,6 +51,7 @@ function calculateValues(loan, term, rate) {
   for (let i = 1; i <= term; i++) {
     // At end each month, Remaining Balance = Previous Remaining Balance - principal payments
     remainingBalance = remainingBalance - principalPayment;
+    // keep from going below zero
     if (remainingBalance <= 0) {
       remainingBalance = 0;
     }
@@ -86,12 +71,13 @@ function calculateValues(loan, term, rate) {
       principal: principalPayment.toFixed(2),
       total: remainingBalance.toFixed(2),
     };
+    // push each month object to the scheduleData array
     scheduleData.push(month);
   }
 
   // add up total costs
   totalCost += totalInterest + loan;
-
+  // creat an object to store the data for the main page header
   let headerData = {
     monthlyPayment: `$${monthlyPayment.toFixed(2)}`,
     totalPrincipal: `$${totalPrincipal.toFixed(2)}`,
@@ -104,8 +90,6 @@ function calculateValues(loan, term, rate) {
 
 //custom display function
 function displayData(scheduleData, headerData) {
-  // change the header Data
-
   // get the monthly payment
   let headerPayment = document.getElementById("monthlyPayment");
   // set the inner HTML to the monthly payment
@@ -125,6 +109,7 @@ function displayData(scheduleData, headerData) {
   let tableBody = document.getElementById("results");
   //get the template row
   let templateRow = document.getElementById("lsTemplate");
+  // get the full table element to change the visibility and show the results
   let fullTable = document.getElementById("table");
   // loop over the array and create a table row for each item.
   for (let i = 0; i < scheduleData.length; i++) {
@@ -133,22 +118,17 @@ function displayData(scheduleData, headerData) {
     let tableRow = document.importNode(templateRow.content, true);
     //   // grab the tds and put them into the array
     let rowCols = tableRow.querySelectorAll("td");
-    //   // add the respective class to each column to change the styling
-    //   // set the data for each column
-    // rowCols[0].classList.add(pageData[i]);
+
+    // set the data for each column
 
     rowCols[0].textContent = scheduleData[i].month;
-    //   //
-    // rowCols[1].classList.add(mortgageData[i + 1][i + 1]);
+
     rowCols[1].textContent = scheduleData[i].payment;
-    //   //
-    // rowCols[2].classList.add(pageData[i + 2][i + 2]);
+
     rowCols[2].textContent = scheduleData[i].interest;
-    //   //
-    // rowCols[3].classList.add(pageData[i + 3][i + 3]);
+
     rowCols[3].textContent = scheduleData[i].principal;
-    //   //
-    // rowCols[4].classList.add(pageData[i + 4][i + 4]);
+
     rowCols[4].textContent = scheduleData[i].total;
 
     // add the template to the document
